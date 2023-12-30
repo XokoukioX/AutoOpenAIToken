a34bfad32c2a0693bcd7abac8cc129fe9f580b30
Kawakaze <52972345+XokoukioX@users.noreply.github.com>
2023-12-30 09:11:59 +0800

    Update autoGetSession.py

--- autoGetSession.py
+++ autoGetSession.py
@@ -10,34 +10,41 @@ import random
 import os
 import re
 
-
 try:
     os.remove("token.txt")
 except FileNotFoundError:
     print("没有Token.txt文件，跳过此步骤")
-a = str(random.randint(1,256))
-b = str(random.randint(5,758))
+a = str(random.randint(1, 256))
+b = str(random.randint(5, 758))
 question = "计算：" + a + "*" + b + "= ?"
 question = str(question)
 print("将发送的问题为:" + question)
 
-user_data_dir = r"C:\Users\zymooll\AppData\Local\Google\Chrome\User Data"
+user_data_dir = r""#这里填写你的Chrome安装目录(e.g C:\Users\your_user_name\AppData\Local\Google\Chrome\User Data)
 
 chrome_options = webdriver.ChromeOptions()
-#chrome_options.add_experimental_option("detach", True)  # 不要自动关闭浏览器
-#chrome_options.add_argument("--disable-blink-features=AutomationControlled")  # 这里添加一些启动的参数
 chrome_options.add_argument('--start-maximized')  # 设置浏览器窗口大小
-#chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])  # 取消chrome受自动控制提示
 chrome_options.add_argument('--profile-directory=Profile 1')
 chrome_options.add_argument(f'--user-data-dir={user_data_dir}')
+
+# chrome_options.add_experimental_option("detach", True)  # 不要自动关闭浏览器
+# chrome_options.add_argument("--disable-blink-features=AutomationControlled")  # 这里添加一些启动的参数
+# chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])  # 取消chrome受自动控制提示
 
 driver = uc.Chrome(options=chrome_options)
 print("打开灌水会话")
 driver.get('https://chat.openai.com/c/74c3bf9f-3396-4aa0-b478-726949a4bdd4')
 print("发送问题")
-WebDriverWait(driver,20).until(EC.visibility_of_element_located((By.XPATH,"/html/body/div[1]/div[1]/div[2]/main/div[2]/div[2]/form/div/div/div/textarea"))).send_keys(question)
+WebDriverWait(driver, 20).until(EC.visibility_of_element_located(
+    (By.XPATH, "/html/body/div[1]/div[1]/div[2]/main/div[2]/div[2]/form/div/div/div/textarea"))).send_keys(question)
 time.sleep(1)
-driver.find_element(By.CSS_SELECTOR,r'#__next > div.relative.z-0.flex.h-full.w-full.overflow-hidden > div.relative.flex.h-full.max-w-full.flex-1.flex-col.overflow-hidden > main > div.flex.h-full.flex-col > div.w-full.pt-2.md\:pt-0.dark\:border-white\/20.md\:border-transparent.md\:dark\:border-transparent.md\:w-\[calc\(100\%-\.5rem\)\] > form > div > div > div > button > span > svg').click()
+driver.find_element(By.CSS_SELECTOR,
+                    r'#__next > div.relative.z-0.flex.h-full.w-full.overflow-hidden > '
+                    r'div.relative.flex.h-full.max-w-full.flex-1.flex-col.overflow-hidden > main > '
+                    r'div.flex.h-full.flex-col > '
+                    r'div.w-full.pt-2.md\:pt-0.dark\:border-white\/20.md\:border-transparent.md\:dark\:border'
+                    r'-transparent.md\:w-\[calc\(100\%-\.5rem\)\] > form > div > div > div > button > span > '
+                    r'svg').click()
 print("等待回复...")
 time.sleep(4)
 print("获取token")
@@ -56,14 +63,12 @@ with open(access_token_file_path, 'w') as file:
     file.write(access_token)
 print("保存为token.txt成功！")
 
-
-file_path = r'C:\Users\zymooll\Desktop\Kawakaze\QBOT_\chatgpt-mirai-qq-bot\config.cfg'
+file_path = r' '#这里填写机器人的cfg位置(e.g C:\Users\your_user_name\Desktop\Kawakaze\QBOT_\chatgpt-mirai-qq-bot\config.cfg)
 # 显式指定使用UTF-8编码打开文件
 with open(file_path, 'r', encoding='utf-8') as file:
     file_content = file.read()
 
 # 在这之后，继续进行操作...
-
 # 定义正则表达式模式，匹配access_token的值
 pattern = re.compile(r'(access_token\s*=\s*")[^"]+(")')
 
@@ -85,5 +90,6 @@ if match:
         file.write(file_content)
 
     print(f'文件 {file_path} 中的 access_token 已被替换。')
+    driver.stop_client()
 else:
     print('未找到匹配的 access_token。')
